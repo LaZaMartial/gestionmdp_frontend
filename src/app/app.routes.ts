e5101a8d253face68854cc65d05d1897b5abcc3f
Ilazasoa Martial ANDRIHERTSE <martialandrihertse@gmail.com>
2025-08-01 10:22:38 +0300

    feat:Add pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/login-page/login-page').then(m => m.LoginPage)
+}, {
+    path: 'home',
+    loadComponent: () => import('./pages/home-page/home-page').then(m => m.HomePage)
+}]; 
